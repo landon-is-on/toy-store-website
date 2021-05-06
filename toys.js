f3307ad259a42929b179be8f5b76ef4260e71d7b
landon-is-on <landonsmothers24@gmail.com>
2021-05-06 13:26:37 -0400

    started to touch api

--- toys.js
+++ toys.js
@@ -15,3 +15,41 @@ cancelBtn.onclick = ()=>{
 window.onscroll = ()=>{
   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
+
+
+import axios from "axios";
+
+const options = {
+  method: 'GET',
+  url: 'https://amazon-product-reviews-keywords.p.rapidapi.com/product/reviews',
+  params: {asin: 'B07XQXZXJC', country: 'US', variants: '1', top: '0'},
+  headers: {
+    'x-rapidapi-key': '85b41c49e7msh15d7722915937e6p134ab0jsnf9615f398a61',
+    'x-rapidapi-host': 'amazon-product-reviews-keywords.p.rapidapi.com'
+  }
+};
+
+
+
+
+
+const form = document.querySelector('#searchForm');
+const button = document.getElementById("#new-toys");
+
+import axios from "axios";
+
+const options = {
+  method: 'GET',
+  url: 'https://amazon-product-reviews-keywords.p.rapidapi.com/product/search',
+  params: {keyword: 'toys', country: 'US', category: 'aps'},
+  headers: {
+    'x-rapidapi-key': '85b41c49e7msh15d7722915937e6p134ab0jsnf9615f398a61',
+    'x-rapidapi-host': 'amazon-product-reviews-keywords.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
