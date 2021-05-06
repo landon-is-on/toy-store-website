4b41aaae7f5533131515e874eea8ef61df1061de
landon-is-on <landonsmothers24@gmail.com>
2021-05-06 16:11:47 -0400

    switched up the api

--- toys.js
+++ toys.js
@@ -15,23 +15,3 @@ cancelBtn.onclick = ()=>{
 window.onscroll = ()=>{
   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
-
-
-
-fetch("https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt1375666", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "85b41c49e7msh15d7722915937e6p134ab0jsnf9615f398a61",
-		"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com"
-	}
-})
-.then(response => response.json()) 
-.then(response => {
-	console.log(response);
-	console.log(response.title);
-})
-.catch(err => {
-	console.error(err);
-});
-
-
