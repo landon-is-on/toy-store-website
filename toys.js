a66021c953c874e49426f1ab6d5f28c79e7b36a4
landon-is-on <landonsmothers24@gmail.com>
2021-05-06 17:43:19 -0400

    got it to work but for some reason it wont let me see the data

--- toys.js
+++ toys.js
@@ -15,3 +15,36 @@ cancelBtn.onclick = ()=>{
 window.onscroll = ()=>{
   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
+
+
+
+const options = {
+  method: 'GET',
+  url: 'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/inception',
+  headers: {
+    'x-rapidapi-key': '85b41c49e7msh15d7722915937e6p134ab0jsnf9615f398a61',
+    'x-rapidapi-host': 'imdb-internet-movie-database-unofficial.p.rapidapi.com'
+  }
+};
+/*
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+*/
+
+form = document.querySelector('#searchForm');
+
+
+    
+    form.addEventListener('submit', async function (e) {
+      e.preventDefault();
+      const searchTerm = form.elements.query.value;
+      const res = await axios.get(`https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${searchTerm}`);
+      console.log(res.data);
+      
+      form.addEventListener('click', async function (e) {
+        console.log(res.data);
+      })
+    })
